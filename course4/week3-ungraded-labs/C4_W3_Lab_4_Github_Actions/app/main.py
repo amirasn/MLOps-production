89e801252569f53cac19320c51a4340acec8235a
Amir Saheb <amirsn@me.com>
2022-05-17 16:41:56 -0700

    Adding new classifier

--- course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
+++ course4/week3-ungraded-labs/C4_W3_Lab_4_Github_Actions/app/main.py
@@ -10,6 +10,7 @@ app = FastAPI(title="Predicting Wine Class with batching")
 #Testing
 
 # Open classifier in global scope
+
 with open("models/wine.pkl", "rb") as file:
     clf = pickle.load(file)
 
